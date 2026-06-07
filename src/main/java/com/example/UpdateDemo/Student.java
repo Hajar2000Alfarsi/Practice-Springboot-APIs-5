44ccbcff5f1c7d38fe409ffbb663ea02cae431f4
Hajar <hajar2000alfarsi@gmail.com>
2026-06-07 10:18:18 +0400

    Complete task 27

--- src/main/java/com/example/UpdateDemo/Student.java
+++ src/main/java/com/example/UpdateDemo/Student.java
@@ -5,4 +5,38 @@ public class Student {
     private String studentName;
     private String grade;
 
+    public Student(String studentName, String grade, int studentId) {
+        this.studentName = studentName;
+        this.grade = grade;
+        this.studentId = studentId;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId=" + studentId +
+                ", studentName='" + studentName + '\'' +
+                ", grade='" + grade + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/example/UpdateDemo/UpdateDemoApplication.java
+++ src/main/java/com/example/UpdateDemo/UpdateDemoApplication.java
@@ -10,8 +10,20 @@ public class UpdateDemoApplication {
 
 		SpringApplication.run(UpdateDemoApplication.class, args);
 
+		//task 26
 		UpdateDemo updateDemo = new UpdateDemo();
 		updateDemo.updateStudentName();
+
+		//task 27
+		Student student = new Student("Ahmed","A",101);
+
+		System.out.println("---Original Student Data---");
+		System.out.println(student);
+
+		student.setStudentName("Ali");
+
+		System.out.println("---After Update---");
+		System.out.println(student);
 	}
 
 
