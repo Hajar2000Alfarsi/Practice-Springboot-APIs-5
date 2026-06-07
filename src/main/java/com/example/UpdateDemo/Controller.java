01d2d0290528d844806ebf3a2e6bf2d16e33dc5f
Hajar <hajar2000alfarsi@gmail.com>
2026-06-07 10:31:45 +0400

    update Student name

--- src/main/java/com/example/UpdateDemo/Controller.java
+++ src/main/java/com/example/UpdateDemo/Controller.java
@@ -1,8 +1,21 @@
 package com.example.UpdateDemo;
 
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
     Student student = new Student("Ahmed","A",101);
+
+    @PutMapping("/updateStudent")
+    public String updateStudent(@RequestParam String name) {
+        String previousName = student.getStudentName();
+
+        student.setStudentName(name);
+
+        return "Student name updated successfully.\n" +
+                "Previous Name: " + previousName + "\n" +
+                "New Name: " + student.getStudentName();
+    }
 }
